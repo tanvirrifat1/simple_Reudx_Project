a9f7604ef13b384dc2d10aa5b800c5f0a7274fad
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-03-05 22:36:55 +0600

    completed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,7 @@
 import { useDispatch } from "react-redux";
 import cancelImage from "../assets/images/cancel.png";
 import { colorSelected, deleted, toggled } from "../redux/todos/actions";
+import deleteTodo from "../redux/todos/thunk/deleteTodo";
 import updateColorChange from "../redux/todos/thunk/updateColor";
 import updatedStatus from "../redux/todos/thunk/updatedStatus";
 
@@ -18,13 +19,13 @@ export default function Todo({ todo }) {
     };
 
     const handleDelete = (todoId) => {
-        dispatch(deleted(todoId));
+        dispatch(deleteTodo(todoId));
     };
 
     return (
         <div className="flex justify-start items-center p-2 hover:bg-gray-100 hover:transition-all space-x-4 border-b border-gray-400/20 last:border-0">
             <div
-                className={`rounded-full bg-white border-2 border-gray-400 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 ${completed &&
+                className={`relative rounded-full bg-white border-2 border-gray-400 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 ${completed &&
                     "border-green-500 focus-within:border-green-500"
                     }`}
             >
