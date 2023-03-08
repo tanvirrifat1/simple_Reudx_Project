311dc17c83dc05f5263c0c5b2800206fb59ee6ae
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-03-08 22:41:05 +0600

    added

---
+++ redux/rtk/features/counter/counterSlice.js
@@ -0,0 +1,15 @@
+const createSlice = require('@reduxjs/toolkit')
+
+const initialState = {
+    count: 0
+}
+
+const counterSlice = createSlice({
+    name: "counter",
+    initialState,
+    reducers: {
+        increment: (state, action) => {
+
+        }
+    }
+})
