134e1a9b8fe16e2069c986d2691dde49acf1add0
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-03-09 20:15:42 +0600

    added toolkit

---
+++ redux/rtk/app/store.js
@@ -0,0 +1,8 @@
+const { configureStore } = require("@reduxjs/toolkit");
+const counterReducer = require("../features/counter/counterSlice")
+
+const store = configureStore({
+    reducer: {
+        counter: counterReducer
+    }
+})
